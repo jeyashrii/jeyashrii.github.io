233d0b419c1f9d8bddbaf3f1586966ad8632467a
jeyashriJ <jeyashri30@gmail.com>
2024-08-25 23:59:04 +0530

    desktop-v

---
+++ projects.js
@@ -0,0 +1,20 @@
+export let projects = [
+  {
+    id: 1,
+    title: "FinTrackr",
+    description:
+      " A smart expense tracker leveraging React, Node.js, and MongoDB to effortlessly manage and analyze your spending habits.",
+  },
+  {
+    id: 2,
+    title: "BattleForge",
+    description:
+      "An action-packed fighting game built with HTML, CSS, and JavaScript, featuring dynamic battles and intuitive controls for a thrilling combat experience right in your browser.",
+  },
+  {
+    id: 2,
+    title: "BingeLog",
+    description:
+      "A sleek movie and show tracker built with the MERN stack, designed to help you organize and keep track of all your watched entertainment effortlessly.",
+  },
+];
---
+++ script.js
@@ -0,0 +1,27 @@
+import { projects } from "./projects.js";
+
+const work = document.querySelector(".work-container");
+function renderProjects() {
+  work.innerHTML = projects
+    .map((project, index) => {
+      const isLast = index === projects.length - 1;
+
+      return `<div class="project">
+    <div class="left"><h2 class="project-title">${project.title}</h2>
+        <p class="project-description">${project.description}</p>
+    </div>
+    <div class="right">
+    <a>Go to Repository</a>
+    
+    </div>
+
+
+  </div>${isLast ? "" : `<hr>`}`;
+    })
+    .join("");
+}
+const copyright = document.querySelector(".copyright");
+const currentYear = new Date().getFullYear();
+copyright.innerHTML = `&copy; ${currentYear}`;
+
+document.addEventListener("DOMContentLoaded", renderProjects);
